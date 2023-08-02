5fd4cc525697c2d84bda7695d191e74d12d07704
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2023-08-02 22:39:05 +0800

    devops: fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import { allTrainingsRaw, clone } from './front/details/trainings';
 import { prepareTrainings } from './front/prepare-trainings';
 import './front/details/style.css';
 
-export function App() {
+export default function RootPage() {
   const allTrainings = clone(allTrainingsRaw);
 
   const filteredTrainings = prepareTrainings(allTrainings);
 
   return <TrainingListPage allTrainings={allTrainings} filteredTrainings={filteredTrainings} />;
 }
-
-export default App;
