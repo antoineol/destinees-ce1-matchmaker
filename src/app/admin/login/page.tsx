735114c5fae1caf8159b0b2c65b8d46eb4357dee
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2023-08-07 13:36:48 +0800

    feat: add acme logo and update login form

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
+import type { FormEvent } from 'react';
 import { useState } from 'react';
 import { login } from '../utils/auth';
+import Image from 'next/image';
+import acmeLogoImg from './acme logo.webp';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
     if (username === 'admin@destinees.co' && password === 'j2b9SY_5DQVb!8hnKN4&Mp$WGdUtSB') {
       login(); // Call the login method
     } else {
@@ -16,31 +20,36 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full bg-white p-8 rounded shadow-md">
+    <div className="min-h-screen flex flex-col items-center justify-center space-y-10 bg-gray-100">
+      <Image src={acmeLogoImg} width={200} alt="Acme" />
+      <form className="max-w-md w-full bg-white p-8 rounded shadow-md" onSubmit={handleLogin}>
         <h2 className="text-2xl mb-4">Login</h2>
         <div className="mb-4">
-          <label className="block mb-1">Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            className="w-full p-2 border rounded"
-          />
+          <label>
+            <div className="block mb-1">Username:</div>
+            <input
+              type="text"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              className="w-full p-2 border rounded"
+            />
+          </label>
         </div>
         <div className="mb-4">
-          <label className="block mb-1">Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            className="w-full p-2 border rounded"
-          />
+          <label>
+            <div className="block mb-1">Password:</div>
+            <input
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              className="w-full p-2 border rounded"
+            />
+          </label>
         </div>
-        <button onClick={handleLogin} className="w-full bg-blue-500 text-white p-2 rounded">
+        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">
           Login
         </button>
-      </div>
+      </form>
     </div>
   );
 };
