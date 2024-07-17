5cb24be82491274c30edcff672bd2a06deb02a0a
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2024-07-17 13:53:02 +0200

    minor ID change for a user and logout event

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 import type { ReactNode } from 'react';
 import { useEffect } from 'react';
-import { useIsAuthenticated } from '../utils/auth';
+import { logout, useIsAuthenticated } from '../utils/auth';
 import Header from './Header';
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -15,6 +15,21 @@ export default function Layout({ children }: { children: ReactNode }) {
       replace('/admin/login');
     }
   }, [isAuth, replace]);
+
+  // This app being embedded as an iframe, let's listen to the parent window's message event "reset" and trigger a logout.
+  useEffect(() => {
+    function handleReset(event: MessageEvent) {
+      if (event.data.type === 'reset') {
+        logout();
+        // Then emit a message logout-done to the parent window
+        window.parent.postMessage({ type: 'reset-done' }, '*');
+      }
+    }
+    window.addEventListener('message', handleReset);
+    return () => {
+      window.removeEventListener('message', handleReset);
+    };
+  }, []);
 
   return (
     <>
